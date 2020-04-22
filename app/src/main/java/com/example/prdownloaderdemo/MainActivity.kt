111312960866d51d4e80189e9c0909caf52048f8
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-04-22 21:52:05 +0545

    Mapped views of MainActivity

--- app/src/main/java/com/example/prdownloaderdemo/MainActivity.kt
+++ app/src/main/java/com/example/prdownloaderdemo/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.prdownloaderdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etURI: EditText
+    private lateinit var btnDownload: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etURI = findViewById(R.id.et_uri)
+        btnDownload = findViewById(R.id.btn_download)
+
+        btnDownload.setOnClickListener {
+            // Start Downloading Here
+        }
     }
 }
